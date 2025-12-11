87204582ff02c28febf57ca16324355de8e6a386
yeweiwei <2013722607@qq.com>
2025-12-11 10:37:09 +0800

    增加vercel insight和google-site-verification

--- src/env.d.ts
+++ src/env.d.ts
@@ -7,3 +7,8 @@ interface Window {
     setTheme: (val: string) => void;
   };
 }
+
+declare module 'astro:env/client' {
+  export const PUBLIC_GOOGLE_SITE_VERIFICATION: string | undefined;
+  export const PUBLIC_GOOGLE_ANALYTICS_ID: string | undefined;
+}
