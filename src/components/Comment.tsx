23980078a9238a4b61d15e6a19654aae6c166182
yeweiwei <2013722607@qq.com>
2025-12-11 16:12:05 +0800

    修改评论组件主题色切换的bug

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -2,11 +2,11 @@ import { useEffect, useState } from "react"
 import { PUBLIC_REMARK_URL } from "astro:env/client";
 
 declare global {
-	// Declare the global types for REMARK42 and remark_config so they can be used in this module.
-	interface Window {
-		REMARK42: any
-		remark_config: any
-	}
+  // Declare the global types for REMARK42 and remark_config so they can be used in this module.
+  interface Window {
+    REMARK42: any
+    remark_config: any
+  }
 }
 
 // Get the host from the environment variables.
@@ -43,18 +43,18 @@ const createOrReinitializeRemark = (theme: string) => {
 
 // Function to insert the Remark42 script into the DOM.
 const insertScript = (id: string, parentElement: HTMLElement) => {
-	const script = window.document.createElement("script")
-	script.type = "text/javascript"
-	script.async = true
-	script.id = id
+  const script = window.document.createElement("script")
+  script.type = "text/javascript"
+  script.async = true
+  script.id = id
 
-	// Get the current URL, and remove trailing slash if it exists.
-	const url = getCurrentUrl();
+  // Get the current URL, and remove trailing slash if it exists.
+  const url = getCurrentUrl();
 
   // Get Blog theme from local storage
   let theme = window.localStorage.getItem('theme') || 'light'
-	// Set the inner HTML of the script to load Remark42.
-	script.innerHTML = `
+  // Set the inner HTML of the script to load Remark42.
+  script.innerHTML = `
     var remark_config = {
       host: "${host}",
       site_id: "${siteId}",
@@ -64,23 +64,23 @@ const insertScript = (id: string, parentElement: HTMLElement) => {
     };
     !function(e,n){for(var o=0;o<e.length;o++){var r=n.createElement("script"),c=".js",d=n.head||n.body;"noModule"in r?(r.type="module",c=".mjs"):r.async=!0,r.defer=!0,r.src=remark_config.host+"/web/"+e[o]+c,d.appendChild(r)}}(remark_config.components||["embed"],document);`
 
-	// Append the script to the parent element.
-	parentElement.appendChild(script)
+  // Append the script to the parent element.
+  parentElement.appendChild(script)
 }
 
 // Function to remove the Remark42 script from the DOM.
 const removeScript = (id: string, parentElement: HTMLElement) => {
-	const script = window.document.getElementById(id)
-	if (script) {
-		parentElement.removeChild(script)
-	}
+  const script = window.document.getElementById(id)
+  if (script) {
+    parentElement.removeChild(script)
+  }
 }
 
 export function Comments() {
-	// State for tracking the current theme.
-	const [theme, setTheme] = useState(() => window.localStorage.getItem("theme") || "light")
+  // State for tracking the current theme.
+  const [theme, setTheme] = useState(() => window.localStorage.getItem("theme") || "light")
 
-	// Effect to handle theme changes and URL changes
+  // Effect to handle theme changes and URL changes
   useEffect(() => {
     createOrReinitializeRemark(theme);
 
@@ -90,10 +90,10 @@ export function Comments() {
     };
   }, [theme, window.location.href]); // Depend on theme and URL to trigger reinitialization
 
-	// 监听 localStorage 中主题的变化
+  // 监听 localStorage 中主题的变化
   useEffect(() => {
-     // 监听 theme-change 事件来切换评论区主题
-		 const handleThemeChange = () => {
+    // 监听 theme-change 事件来切换评论区主题
+    const handleThemeChange = () => {
       const newTheme = window.localStorage.getItem('theme') || 'light';
       setTheme(newTheme);
     };
@@ -105,22 +105,22 @@ export function Comments() {
     };
   }, [theme]);
 
-	// Use effect to update the theme when it changes.
-	useEffect(() => {
-		if (window.REMARK42) {
-			window.REMARK42.changeTheme(theme)
-		}
-	}, [theme])
+  // Use effect to update the theme when it changes.
+  useEffect(() => {
+    if (window.REMARK42) {
+      window.REMARK42.changeTheme(theme)
+    }
+  }, [theme])
 
-	return (
-		<>
-			<div className="mt-10 mb-5 border-t-2 pt-3">
+  return (
+    <>
+      <div className="mt-10 mb-5 border-t-2 pt-3">
         <span className="text-2xl pr-1 font-extralight">Comments</span>
         <span className="counter align-super font-bold">
           <span className="remark42__counter"></span>
         </span>
       </div>
       <div id="remark42"></div>
-		</>
-	)
+    </>
+  )
 }
